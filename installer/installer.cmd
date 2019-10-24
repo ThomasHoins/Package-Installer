072bd360f5fffe7c8fd94d92dc6ccf4a12f104b8
ThomasHoins <thomas.hoins@web.de>
2019-10-24 12:43:05 +0200

    Minor GUI fixes, Fix at Installer.cmd, New Logo handling, Error Handling for MIF and Reg creation

--- installer/installer.cmd
+++ installer/installer.cmd
@@ -1,14 +1,15 @@
-@echo off
+@ECHO OFF
 IF %Username% EQU %COMPUTERNAME%$ goto SYSTEM
-IF "%Username%" NEQ "%COMPUTERNAME%" goto User
-goto EXIT
+IF "%Username%" NEQ "%COMPUTERNAME$%" goto USER
+GOTO EXIT
 
 :SYSTEM
-START "Installer" /I /MIN "%~dp0\Resources\ServiceUI.exe" -process:explorer.exe "%windir%\System32\WindowsPowerShell\v1.0\powershell.exe" -WindowStyle "hidden" -ExecutionPolicy "ByPass" "%~dp0Resources\Installer.ps1" %1 %2
-goto EXIT
+START "Installer" /I /MIN "%~dp0\Resources\ServiceUI.exe" -process:explorer.exe "%windir%\System32\WindowsPowerShell\v1.0\powershell.exe" -WindowStyle "hidden" -ExecutionPolicy "ByPass" -File "%~dp0Resources\Installer.ps1" %1 %2
+GOTO EXIT
 
-:User
-START "Installer" /I /MIN "%windir%\System32\WindowsPowerShell\v1.0\powershell.exe" -WindowStyle "hidden" -ExecutionPolicy "ByPass" "%~dp0Resources\Installer.ps1" %1 %2
-goto EXIT
+:USER
+START "Installer" /I /MIN "%windir%\System32\WindowsPowerShell\v1.0\powershell.exe" -WindowStyle "hidden" -ExecutionPolicy "ByPass" -File "%~dp0Resources\Installer.ps1" %1 %2
+GOTO EXIT
 
 :EXIT	
+
