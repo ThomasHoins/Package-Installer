bf82901d7274c9920639431bd6e7a644e649e311
ThomasHoins <thomas.hoins@web.de>
2019-12-02 10:24:05 +0100

    changed installer.cmd

--- installer/installer.cmd
+++ installer/installer.cmd
@@ -1,17 +1,38 @@
 @ECHO OFF
-"%~dp0\Resources\CONSOLESTATE.exe" /Hide
+"%~dp0Resources\CONSOLESTATE.exe" /Hide
+Set logpath=C:\program files\Support\Logs
+Set logfile="%logpath%\installer.log"
+Set OSDSETUPHOOK=tasklist /FI "IMAGENAME eq OSDSETUPHOOK.exe"
 
-IF %Username% EQU %COMPUTERNAME%$ goto SYSTEM
-IF "%Username%" NEQ "%COMPUTERNAME$%" goto USER
+ECHO -------------------------------------------------- >> %logfile%
+ECHO Starting Installation %date% %time% >> %logfile%
+
+IF DEFINED OSDSETUPHOOK GOTO TASKSEQUENCE
+IF %Username% EQU %COMPUTERNAME%$ GOTO SYSTEM
+IF "%Username%" NEQ "%COMPUTERNAME%$" GOTO USER
 GOTO EXIT
 
 :SYSTEM
-START "Installer" /I /MIN /WAIT "%~dp0\Resources\ServiceUI.exe" -process:explorer.exe "%windir%\System32\WindowsPowerShell\v1.0\powershell.exe" -WindowStyle "hidden" -ExecutionPolicy "ByPass" -File "%~dp0Resources\Installer.ps1" %1 %2
+ECHO "SYSTEM" >> %logfile%
+ECHO Launching: START "Installer" /I /MIN /WAIT "%~dp0Resources\ServiceUI.exe" -process:explorer.exe "%windir%\System32\WindowsPowerShell\v1.0\powershell.exe" -WindowStyle "hidden" -ExecutionPolicy "ByPass" -File "%~dp0Resources\Installer.ps1" %1 %2 >> %logfile%
+START "Installer" /I /MIN /WAIT "%~dp0Resources\ServiceUI.exe" -process:explorer.exe "%windir%\System32\WindowsPowerShell\v1.0\powershell.exe" -WindowStyle "hidden" -ExecutionPolicy "ByPass" -File "%~dp0Resources\Installer.ps1" %1 %2 >> %logfile%
+GOTO EXIT
+
+:TASKSEQUENCE
+ECHO "TASKSEQUENCE" >> %logfile%
+ECHO Launching: START "Installer" /I /MIN /WAIT "%windir%\System32\WindowsPowerShell\v1.0\powershell.exe" -WindowStyle "hidden" -ExecutionPolicy "ByPass" -File "%~dp0Resources\Installer.ps1" %1 %2 >> %logfile%
+START "Installer" /I /MIN /WAIT "%~dp0Resources\ServiceUI.exe" -process:TSProgressUI.exe "%windir%\System32\WindowsPowerShell\v1.0\powershell.exe" -WindowStyle "hidden" -ExecutionPolicy "ByPass" -File "%~dp0Resources\Installer.ps1" %1 %2 >> %logfile%
 GOTO EXIT
 
 :USER
-START "Installer" /I /MIN /WAIT "%windir%\System32\WindowsPowerShell\v1.0\powershell.exe" -WindowStyle "hidden" -ExecutionPolicy "ByPass" -File "%~dp0Resources\Installer.ps1" %1 %2
+ECHO "USER" >> %logfile%
+ECHO Launching: START "Installer" /I /MIN /WAIT "%windir%\System32\WindowsPowerShell\v1.0\powershell.exe" -WindowStyle "hidden" -ExecutionPolicy "ByPass" -File "%~dp0Resources\Installer.ps1" %1 %2 >> %logfile%
+START "Installer" /I /MIN /WAIT "%windir%\System32\WindowsPowerShell\v1.0\powershell.exe" -WindowStyle "hidden" -ExecutionPolicy "ByPass" -File "%~dp0Resources\Installer.ps1" %1 %2 >> %logfile%
 GOTO EXIT
 
-:EXIT	
 
+:EXIT	
+ECHO Ending Installation  %date% %time% >> %logfile%
+ECHO -------------------------------------------------- >> %logfile%
+ECHO. >> %logfile%
+"%~dp0Resources\CONSOLESTATE.exe" /Show
