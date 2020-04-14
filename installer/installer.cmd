a36740b1a67e85440759ea7172ac97cc2819d058
TomTom1492 <55402211+ThomasHoins@users.noreply.github.com>
2020-04-14 12:03:40 +0200

    some changes to installer.log and xml path
    changed set logpath to
    Set logpath=%temp%
    due to restrictions with per user only installations.
    replaced %2 through "%~dp0%2"
    SCCM did not pass the complete xml path through, so we are fixing it here.

--- installer/installer.cmd
+++ installer/installer.cmd
@@ -1,6 +1,6 @@
 @ECHO OFF
 "%~dp0Resources\CONSOLESTATE.exe" /Hide
-Set logpath=C:\program files\Support\Logs
+Set logpath=%temp%
 Set logfile="%logpath%\installer.log"
 For /F "Tokens=*" %%I in ('tasklist /FI "IMAGENAME eq OSDSETUPHOOK.exe" ^| find /I "OSDSETUPHOOK.exe"') Do Set OSDSETUPHOOK=%%I
 
@@ -14,20 +14,20 @@ GOTO EXIT
 
 :SYSTEM
 ECHO "SYSTEM" >> %logfile%
-ECHO Launching: START "Installer" /I /MIN /WAIT "%~dp0Resources\ServiceUI.exe" -process:explorer.exe "%windir%\System32\WindowsPowerShell\v1.0\powershell.exe" -WindowStyle "hidden" -ExecutionPolicy "ByPass" -File "%~dp0Resources\Installer.ps1" %1 %2 >> %logfile%
-START "Installer" /I /MIN /WAIT "%~dp0Resources\ServiceUI.exe" -process:explorer.exe "%windir%\System32\WindowsPowerShell\v1.0\powershell.exe" -WindowStyle "hidden" -ExecutionPolicy "ByPass" -File "%~dp0Resources\Installer.ps1" %1 %2 >> %logfile%
+ECHO Launching: START "Installer" /I /MIN /WAIT "%~dp0Resources\ServiceUI.exe" -process:explorer.exe "%windir%\System32\WindowsPowerShell\v1.0\powershell.exe" -WindowStyle "hidden" -ExecutionPolicy "ByPass" -File "%~dp0Resources\Installer.ps1" %1  "%~dp0%2" >> %logfile%
+START "Installer" /I /MIN /WAIT "%~dp0Resources\ServiceUI.exe" -process:explorer.exe "%windir%\System32\WindowsPowerShell\v1.0\powershell.exe" -WindowStyle "hidden" -ExecutionPolicy "ByPass" -File "%~dp0Resources\Installer.ps1" %1  "%~dp0%2" >> %logfile%
 GOTO EXIT
 
 :TASKSEQUENCE
 ECHO "TASKSEQUENCE" >> %logfile%
-ECHO Launching: START "Installer" /I /MIN /WAIT "%windir%\System32\WindowsPowerShell\v1.0\powershell.exe" -WindowStyle "hidden" -ExecutionPolicy "ByPass" -File "%~dp0Resources\Installer.ps1" %1 %2 >> %logfile%
-START "Installer" /I /MIN /WAIT "%~dp0Resources\ServiceUI.exe" -process:TSProgressUI.exe "%windir%\System32\WindowsPowerShell\v1.0\powershell.exe" -WindowStyle "hidden" -ExecutionPolicy "ByPass" -File "%~dp0Resources\Installer.ps1" %1 %2 >> %logfile%
+ECHO Launching: START "Installer" /I /MIN /WAIT "%windir%\System32\WindowsPowerShell\v1.0\powershell.exe" -WindowStyle "hidden" -ExecutionPolicy "ByPass" -File "%~dp0Resources\Installer.ps1" %1  "%~dp0%2" >> %logfile%
+START "Installer" /I /MIN /WAIT "%~dp0Resources\ServiceUI.exe" -process:TSProgressUI.exe "%windir%\System32\WindowsPowerShell\v1.0\powershell.exe" -WindowStyle "hidden" -ExecutionPolicy "ByPass" -File "%~dp0Resources\Installer.ps1" %1  "%~dp0%2" >> %logfile%
 GOTO EXIT
 
 :USER
 ECHO "USER" >> %logfile%
-ECHO Launching: START "Installer" /I /MIN /WAIT "%windir%\System32\WindowsPowerShell\v1.0\powershell.exe" -WindowStyle "hidden" -ExecutionPolicy "ByPass" -File "%~dp0Resources\Installer.ps1" %1 %2 >> %logfile%
-START "Installer" /I /MIN /WAIT "%windir%\System32\WindowsPowerShell\v1.0\powershell.exe" -WindowStyle "hidden" -ExecutionPolicy "ByPass" -File "%~dp0Resources\Installer.ps1" %1 %2 >> %logfile%
+ECHO Launching: START "Installer" /I /MIN /WAIT "%windir%\System32\WindowsPowerShell\v1.0\powershell.exe" -WindowStyle "hidden" -ExecutionPolicy "ByPass" -File "%~dp0Resources\Installer.ps1" %1 "%~dp0%2" >> %logfile%
+START "Installer" /I /MIN /WAIT "%windir%\System32\WindowsPowerShell\v1.0\powershell.exe" -WindowStyle "hidden" -ExecutionPolicy "ByPass" -File "%~dp0Resources\Installer.ps1" %1 "%~dp0%2" >> %logfile%
 GOTO EXIT
 
 
