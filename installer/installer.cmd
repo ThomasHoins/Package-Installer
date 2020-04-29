2800f9aedafa026f34a2c838199d0e0a37847268
captain <captain.onboard@web.de>
2020-04-29 17:49:10 +0200

    [installer.cmd] fix tasksequence install part add one click installer

--- installer/installer.cmd
+++ installer/installer.cmd
@@ -4,6 +4,17 @@ Set logpath=%temp%
 Set logfile="%logpath%\installer.log"
 For /F "Tokens=*" %%I in ('tasklist /FI "IMAGENAME eq OSDSETUPHOOK.exe" ^| find /I "OSDSETUPHOOK.exe"') Do Set OSDSETUPHOOK=%%I
 
+if #%1# == ## (
+  set MODE=/i
+) else (
+  set MODE=%1
+)
+if #%2# == ## (
+  set INSTALLER="%~dp0installer.xml"
+) else (
+  set INSTALLER="%~dp0%2"
+)
+
 ECHO -------------------------------------------------- >> %logfile%
 ECHO Starting Installation %date% %time% >> %logfile%
 
@@ -14,24 +25,23 @@ GOTO EXIT
 
 :SYSTEM
 ECHO "SYSTEM" >> %logfile%
-ECHO Launching: START "Installer" /I /MIN /WAIT "%~dp0Resources\ServiceUI.exe" -process:explorer.exe "%windir%\System32\WindowsPowerShell\v1.0\powershell.exe" -WindowStyle "hidden" -ExecutionPolicy "ByPass" -File "%~dp0Resources\Installer.ps1" %1  "%~dp0%2" >> %logfile%
-START "Installer" /I /MIN /WAIT "%~dp0Resources\ServiceUI.exe" -process:explorer.exe "%windir%\System32\WindowsPowerShell\v1.0\powershell.exe" -WindowStyle "hidden" -ExecutionPolicy "ByPass" -File "%~dp0Resources\Installer.ps1" %1  "%~dp0%2" >> %logfile%
+ECHO Launching: START "Installer" /I /MIN /WAIT "%~dp0Resources\ServiceUI.exe" -process:explorer.exe "%windir%\System32\WindowsPowerShell\v1.0\powershell.exe" -WindowStyle "hidden" -ExecutionPolicy "ByPass" -File "%~dp0Resources\Installer.ps1" %MODE% %INSTALLER% >> %logfile%
+START "Installer" /I /MIN /WAIT "%~dp0Resources\ServiceUI.exe" -process:explorer.exe "%windir%\System32\WindowsPowerShell\v1.0\powershell.exe" -WindowStyle "hidden" -ExecutionPolicy "ByPass" -File "%~dp0Resources\Installer.ps1" %MODE% %INSTALLER% >> %logfile%
 GOTO EXIT
 
 :TASKSEQUENCE
 ECHO "TASKSEQUENCE" >> %logfile%
-ECHO Launching: START "Installer" /I /MIN /WAIT "%windir%\System32\WindowsPowerShell\v1.0\powershell.exe" -WindowStyle "hidden" -ExecutionPolicy "ByPass" -File "%~dp0Resources\Installer.ps1" %1  "%~dp0%2" >> %logfile%
-START "Installer" /I /MIN /WAIT "%~dp0Resources\ServiceUI.exe" -process:TSProgressUI.exe "%windir%\System32\WindowsPowerShell\v1.0\powershell.exe" -WindowStyle "hidden" -ExecutionPolicy "ByPass" -File "%~dp0Resources\Installer.ps1" %1  "%~dp0%2" >> %logfile%
+ECHO Launching: START "Installer" /I /MIN /WAIT "%windir%\System32\WindowsPowerShell\v1.0\powershell.exe" -WindowStyle "hidden" -ExecutionPolicy "ByPass" -File "%~dp0Resources\Installer.ps1" %1 %2 >> %logfile%
+START "Installer" /I /MIN /WAIT "%windir%\System32\WindowsPowerShell\v1.0\powershell.exe" -WindowStyle "hidden" -ExecutionPolicy "ByPass" -File "%~dp0Resources\Installer.ps1" %1 %2 >> %logfile%
 GOTO EXIT
 
 :USER
 ECHO "USER" >> %logfile%
-ECHO Launching: START "Installer" /I /MIN /WAIT "%windir%\System32\WindowsPowerShell\v1.0\powershell.exe" -WindowStyle "hidden" -ExecutionPolicy "ByPass" -File "%~dp0Resources\Installer.ps1" %1 "%~dp0%2" >> %logfile%
-START "Installer" /I /MIN /WAIT "%windir%\System32\WindowsPowerShell\v1.0\powershell.exe" -WindowStyle "hidden" -ExecutionPolicy "ByPass" -File "%~dp0Resources\Installer.ps1" %1 "%~dp0%2" >> %logfile%
+ECHO Launching: START "Installer" /I /MIN /WAIT "%windir%\System32\WindowsPowerShell\v1.0\powershell.exe" -WindowStyle "hidden" -ExecutionPolicy "ByPass" -File "%~dp0Resources\Installer.ps1" %MODE% %INSTALLER% >> %logfile%
+START "Installer" /I /MIN /WAIT "%windir%\System32\WindowsPowerShell\v1.0\powershell.exe" -WindowStyle "hidden" -ExecutionPolicy "ByPass" -File "%~dp0Resources\Installer.ps1" %MODE% %INSTALLER% >> %logfile%
 GOTO EXIT
 
-
-:EXIT	
+:EXIT
 ECHO Ending Installation  %date% %time% >> %logfile%
 ECHO -------------------------------------------------- >> %logfile%
 ECHO. >> %logfile%
